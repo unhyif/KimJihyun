d55445bc24e60294bf9871539321b5b23cd611f8
Jihyun Kim <ang21icjh@gmail.com>
2022-01-05 19:01:52 +0900

    3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,11 @@
 num = 0
-called_num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+while True:
+    called_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    if called_num.isdigit(): # 0 이상의 정수를 입력받았을 때
+        if 1 <= int(called_num) <= 3: # 1 이상 3 이하 정수일 때
+            called_num = int(called_num)
+            break
+        else: # 0 또는 3 초과인 정수일 때
+            print("1,2,3 중 하나를 입력하세요")
+    else: # 음수, 소수, 문자를 입력받았을 때
+        print("정수를 입력하세요")
