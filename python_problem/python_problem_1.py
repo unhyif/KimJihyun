1a5e09326843b7465c3e32c2731714072433b343
Jihyun Kim <ang21icjh@gmail.com>
2022-01-05 20:05:43 +0900

    8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,40 @@
 players = ["playerA", "playerB"] # player 목록
-main_player = "" # 숫자를 부를 차례인 player
-num = 0 # 현재까지 불러진 수
-current_turn = 0 # 현재 몇번째 turn이 진행 중인지
+def brGame(players_list):
+    global players # 마지막에 승자 결정 시 players를 수정하기 위해
+    
+    main_player = "" # 숫자를 부를 차례인 player
 
-while num < 31:
+    num = 0 # 현재까지 불러진 수
+    current_turn = 0 # 현재 몇번째 turn이 진행 중인지
 
-    current_turn += 1
-    if current_turn % 2 == 1: # 홀수번째 turn이면
-        main_player = players[0] # player1 차례
-    else: # 짝수번째 turn이면
-        main_player = players[1] # player2 차례
+    while num < 31:
 
-    while True: # 1 이상 3 이하의 정수를 입력받는 과정
-        called_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        current_turn += 1
+        if current_turn % 2 == 1: # 홀수번째 turn이면
+            main_player = players[0] # player1 차례
+        else: # 짝수번째 turn이면
+            main_player = players[1] # player2 차례
 
-        if called_num.isdigit(): # 0 이상의 정수를 입력받았을 때
-            if 1 <= int(called_num) <= 3: # 1 이상 3 이하 정수일 때
-                called_num = int(called_num)
+        while True: # 1 이상 3 이하의 정수를 입력받는 과정
+            called_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
+            if called_num.isdigit(): # 0 이상의 정수를 입력받았을 때
+                if 1 <= int(called_num) <= 3: # 1 이상 3 이하 정수일 때
+                    called_num = int(called_num)
+                    break
+                else: # 0 또는 3 초과인 정수일 때
+                    print("1,2,3 중 하나를 입력하세요")
+            else: # 음수, 소수, 문자를 입력받았을 때
+                print("정수를 입력하세요")
+
+        for _ in range(called_num):
+            num += 1
+            print(f"{main_player} :", num)
+            if num == 31:
+                players.remove(main_player) # 승자만 players에 남겨 놓기 위해
                 break
-            else: # 0 또는 3 초과인 정수일 때
-                print("1,2,3 중 하나를 입력하세요")
-        else: # 음수, 소수, 문자를 입력받았을 때
-            print("정수를 입력하세요")
 
-    for _ in range(called_num):
-        num += 1
-        print(f"{main_player} :", num)
-        if num == 31:
-            players.remove(main_player) # 승자만 players에 남겨 놓기 위해
-            break
+    print(f"{players[0]} win!")
 
 
-print(f"{players[0]} win!")
+brGame(players)
