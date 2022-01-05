6d5082472322685c33ced52fb00ea0bb503ac7b2
Jihyun Kim <ang21icjh@gmail.com>
2022-01-05 19:43:27 +0900

    4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,9 @@
-num = 0
-while True:
+players = ["playerA", "playerB"] # player 목록
+num = 0 # 현재까지 불러진 수
+
+while True: # 1 이상 3 이하의 정수를 입력받는 과정
     called_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+
     if called_num.isdigit(): # 0 이상의 정수를 입력받았을 때
         if 1 <= int(called_num) <= 3: # 1 이상 3 이하 정수일 때
             called_num = int(called_num)
@@ -9,3 +12,7 @@ while True:
             print("1,2,3 중 하나를 입력하세요")
     else: # 음수, 소수, 문자를 입력받았을 때
         print("정수를 입력하세요")
+
+for _ in range(called_num):
+    num += 1
+    print(f"{players[0]} :", num)
