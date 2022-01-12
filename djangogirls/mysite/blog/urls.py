7868fb87a5770b88e8b925d9fc4c39e8005ecbdf
Jihyun Kim <ang21icjh@gmail.com>
2022-01-12 15:08:29 +0900

    애플리케이션 확장하기

--- djangogirls/mysite/blog/urls.py
+++ djangogirls/mysite/blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # home에서 views.post_list 보여줌, url name 설정
+    path('post/<int:pk>/', views.post_detail, name='post_detail') # 정수를 받으면 pk로 인식
 ]
--- djangogirls/mysite/blog/views.py
+++ djangogirls/mysite/blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # Queryset
     return render(request, 'blog/post_list.html', {'posts': posts}) # model에서 필요한 정보를 받아 template에 전달, 오른쪽 posts는 parameter
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
