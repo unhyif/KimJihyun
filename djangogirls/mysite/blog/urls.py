4b13f5eb50e96486780986f6700a1f41634405f5
Jihyun Kim <ang21icjh@gmail.com>
2022-01-12 15:46:38 +0900

    Django 폼

--- djangogirls/mysite/blog/urls.py
+++ djangogirls/mysite/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # home에서 views.post_list 보여줌, url name 설정
-    path('post/<int:pk>/', views.post_detail, name='post_detail') # 정수를 받으면 pk로 인식
+    path('post/<int:pk>/', views.post_detail, name='post_detail'), # 정수를 받으면 pk로 인식
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
--- djangogirls/mysite/blog/views.py
+++ djangogirls/mysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') # Queryset
@@ -9,3 +11,30 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() # 변경사항 반영 후 저장 -> 커밋
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post) # request.POST에 정보 담김
+        if form.is_valid():
+            post = form.save(commit=False) # post 새로 저장
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post) # 이전 글
+    return render(request, 'blog/post_edit.html', {'form': form})
