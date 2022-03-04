f671026b2cf237637cb3e5b4d610638fcb4b887a
Jihyun Kim <ang21icjh@gmail.com>
2022-03-05 04:12:16 +0900

    refactor: remove repeated if statement & rename variables

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,29 +1,33 @@
 from random import *
 players = ["computer", "player"] # player 목록
 
-def brGame(players_list):
-    main_player = "" # 숫자를 부를 차례인 player
-    num = 0 # 현재까지 불러진 수
+
+def play_br_game(players):
+    print("computer, player 순으로 번갈아 가며 게임이 진행됩니다.")
+
+    main_player = None # 숫자를 부를 차례인 player
+    last_num = 0 # 현재까지 불러진 수
     current_turn = 0 # 현재 몇번째 turn이 진행 중인지
 
-    while num < 31:
-
+    while last_num < 31:
         current_turn += 1
-        if current_turn % 2: # 홀수번째 turn이면
-            main_player = players[0] # computer 차례
-        else: # 짝수번째 turn이면
-            main_player = players[1] # player 차례
 
-        if main_player == players[1]: # player 차례면 숫자를 입력 받음
-            while True: # 1 이상 3 이하의 정수를 입력받는 과정
-                called_num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-                
+        if current_turn % 2: # 홀수번째 turn엔
+            main_player = players[0] # computer가 랜덤으로 숫자 부름
+            shouted_num = randint(1, 3)
+
+        else: # 짝수번째 turn엔
+            main_player = players[1] # player로부터 숫자를 입력 받음
+
+            while True:
                 try:
-                    called_num = float(called_num)
-                    if called_num % 1:
+                    shouted_num = float(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                
+                    if shouted_num % 1:
                         raise
-                    if 1 <= int(called_num) <= 3: # 1 이상 3 이하 정수일 때
-                        called_num = int(called_num)
+
+                    if 1 <= int(shouted_num) <= 3: # 1 이상 3 이하 정수일 때
+                        shouted_num = int(shouted_num)
                         break
                     else: # 그 외 정수를 입력받았을 때
                         print("1,2,3 중 하나를 입력하세요")
@@ -31,17 +35,16 @@ def brGame(players_list):
                 except: # 소수, 문자를 입력받았을 때
                     print("정수를 입력하세요")
 
-        else: # computer 차례면 랜덤으로 숫자 부름
-            called_num = randint(1, 3)
 
-        for _ in range(called_num):
-            num += 1
-            print(f"{main_player} :", num)
-            if num == 31:
-                players.remove(main_player) # 승자만 players에 남겨 놓기 위해
+        for count in range(shouted_num):
+            last_num += 1
+            print(f"{main_player} : {last_num}")
+
+            if last_num == 31:
+                players.remove(main_player)
                 break
 
-    print(f"{players[0]} win!")
+    print(f"{players[0]} won!")
 
 
-brGame(players)
+play_br_game(players)
